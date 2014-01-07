641e1b23a225b4b47c6608bc8b86a3d2ae9e9357
pranavbhalla89 <Apple@Apples-MacBook-Pro.local>
2014-01-07 04:43:36 -0500

    done wid d 8

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
-    password "foobar"
-    password_confirmation "foobar"
+    name     "Pranav"
+    email    "p@b.com"
+    password "pwdpwd"
+    password_confirmation "pwdpwd"
   end
 end
