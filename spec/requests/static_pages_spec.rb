f739618fef65e9e68d36ecedebe8384ab98e2aa4
pranavbhalla89 <Apple@Apples-MacBook-Pro.local>
2014-01-08 06:42:42 -0500

    ch 10 done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,6 +22,22 @@ describe "StaticPages" do
     	visit '/static_pages/home'
     	expect(page).to have_title("#{base_title}")
     end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "what the hell ??")
+        FactoryGirl.create(:micropost, user: user, content: "this is sparta...")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li#{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
